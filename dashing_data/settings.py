01843f8af40f2ec8f8fb140e99c3f74fddd386b0
Ollie Power <powerollie@gmail.com>
2020-02-18 22:24:26 +0000

    Added crispy forms

--- dashing_data/settings.py
+++ dashing_data/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'upload.apps.UploadConfig',
 ]
 
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- upload/forms.py
+++ upload/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 
 class UploadDataForm(forms.Form):
-    chart_title = forms.CharField(label='Chart Title', max_length=100, required=True)
+    chart_title = forms.CharField(
+        label='Chart Title',
+        max_length=100,
+        required=True,
+    )
 
 # https://docs.djangoproject.com/en/3.0/ref/forms/api/#binding-uploaded-files
 # https://docs.djangoproject.com/en/3.0/ref/forms/fields/
