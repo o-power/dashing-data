380dada3cc99b224dad8e2143c35300e4f2945ec
Ollie Power <powerollie@gmail.com>
2020-02-27 22:09:14 +0000

    Removing old upload app

--- dashing_data/urls.py
+++ dashing_data/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import urls as urls_home
-from upload import urls as urls_upload
 from barchart import urls as urls_barchart
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(urls_home)),
-    path('upload/', include(urls_upload)),
     path('barchart/', include(urls_barchart)),
 ]
--- upload/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import upload
-
-urlpatterns = [
-    path('', upload, name='upload'),
-]
