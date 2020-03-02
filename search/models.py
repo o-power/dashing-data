7a40ee0487f0de49ac95c504670d2db3b218fedb
Ollie Power <powerollie@gmail.com>
2020-03-02 23:04:59 +0000

    Renamed UserCharts to UserChart

--- search/models.py
+++ search/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserCharts(models.Model):
+class UserChart(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     chart_type = models.CharField(max_length=50)
     title = models.CharField(max_length=255)
