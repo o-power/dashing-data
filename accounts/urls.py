784fd2bd3968e4aeb1169188b116d07c163be139
Ollie Power <powerollie@gmail.com>
2020-03-06 22:50:15 +0000

    Added redirect to accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from .views import register, profile, logout, login
 from . import urls_reset
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='register/')),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout'),
