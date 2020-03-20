9dff8b57a25f31d010e9bc31adb00e9f01b8562b
Ollie Power <powerollie@gmail.com>
2020-03-20 16:06:24 +0000

    Fixed env.py import to always happen if the file is present

--- dashing_data/settings.py
+++ dashing_data/settings.py
@@ -14,9 +14,10 @@ import os
 from django.contrib.messages import constants as messages
 import dj_database_url
 
-DEVELOPMENT = os.environ.get('DEVELOPMENT') == 'TRUE'
-if DEVELOPMENT:
+if os.path.exists('env.py'):
     import env
+
+DEVELOPMENT = os.environ.get('DEVELOPMENT') == 'TRUE'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
