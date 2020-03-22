649012c5dcff363c166029e4754e67cc91bc6bb4
Ollie Power <powerollie@gmail.com>
2020-03-22 09:08:10 +0000

    Added table to barchart page

---
+++ static/js/barchart.js
@@ -0,0 +1,47 @@
+// set the dimensions and margins of the graph
+var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+width = 460 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#draw-here")
+.append("svg")
+//.attr("width", width + margin.left + margin.right)
+//.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform",
+  "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+let data = JSON.parse(document.getElementById('bar_data').textContent);;
+
+console.log(data)
+// X axis
+var x = d3.scaleBand()
+.range([0, width])
+.domain(data.map(function (d) { return d.x_data; }))
+.padding(0.2);
+svg.append("g")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x))
+.selectAll("text")
+.attr("transform", "translate(-10,0)rotate(-45)")
+.style("text-anchor", "end");
+
+// Add Y axis
+var y = d3.scaleLinear()
+.domain([0, 10])
+.range([height, 0]);
+svg.append("g")
+.call(d3.axisLeft(y));
+
+// Bars
+svg.selectAll("mybar")
+.data(data)
+.enter()
+.append("rect")
+.attr("x", function (d) { return x(d.x_data); })
+.attr("y", function (d) { return y(d.y_data); })
+.attr("width", x.bandwidth())
+.attr("height", function (d) { return height - y(d.y_data); })
+.attr("fill", "#69b3a2");
